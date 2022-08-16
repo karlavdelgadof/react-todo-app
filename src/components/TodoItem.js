9b78f3b9a395a3b96d5a1cf267c0fc20921b0692
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-16 02:27:03 -0400

    Implement modular CSS react for todos items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,33 @@
 import React from "react";
-
+import styles from "./TodoItem.module.css";
 class TodoItem extends React.Component {
   render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    };
+
+    const { completed, id, title } = this.props.todo
+
     return (
-      <li> 
-        <input 
-          type="checkbox" 
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+      <li className={styles.item}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
         />
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        <button onClick={() => this.props.deleteTodoProps(id)}>
           Delete
         </button>
-        {this.props.todo.title}
+        <span style={this.props.todo.completed ? completedStyle : null}>
+          {title}
+        </span>
       </li>
     );
   }
 }
 
-export default TodoItem
+export default TodoItem;
